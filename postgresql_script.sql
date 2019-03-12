4833813e58af48dacbb26e3040274c4727884a23
kelvinnpc <kelvinnpc@gmail.com>
2019-03-12 14:03:31 +0800

    update postgresql_script

--- postgresql_script.sql
+++ postgresql_script.sql
@@ -1,37 +1,37 @@
 CREATE TABLE Users (
-	ic  varchar(9) PRIMARY KEY,
 	name    varchar(255) NOT NULL,
 	username varchar(255) NOT NULL,
 	password varchar(255) NOT NULL,
+	nric  varchar(9) PRIMARY KEY,
 	phoneNumber int NOT NULL
 );
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000001A', 'Leslie Cole', 'LeslieCole', 'password1', 12345678);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000002B', 'Myra Morgan', 'MyraMorgan', 'password1',12345677);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000003C', 'Raymond Benson', 'RaymondBenson', 'password1', 12345676);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000004D', 'Wendy Kelley', 'WendyKelley', 'password1', 12345675);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000005E', 'Patrick Bowers', 'PatrickBowers', 'password1', 12345674);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000006F', 'Ralph Hogan', 'RalphHogan', 'password1', 12345673);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000007G', 'Cecil Rodriquez', 'CecilRodriquez', 'password1', 12345672);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000008H', 'Delia Ferguson', 'DeliaFerguson', 'password1', 12345671);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000009I', 'Frances Wright', 'FrancesWright', 'password1', 12345670);
 
-INSERT INTO Users (ic, name, username, password, phoneNumber)
+INSERT INTO Users (name, username, password, nric, phoneNumber)
 VALUES ('S0000010J', 'Alyssa Sims', 'AlyssaSims', 'password1', 12345681);
